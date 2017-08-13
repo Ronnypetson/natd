17119cb0c1bb320a0cee219e15f6bc4384f65c0e
Ronnypetson <rsronnypetson4@gmail.com>
2017-08-13 19:50:14 -0300

    terceiro commit

--- instancies.py
+++ instancies.py
@@ -2,7 +2,7 @@ from random import randint
 from prop import genProp, getArgs, Rule
 
 prop_len = 10
-depth_param = 5 # 9: d ~ 7, w ~ 4
+depth_param = 4 # 9: d ~ 7, w ~ 4
 
 root_prop = genProp(randint(5,prop_len))
 prop_stack = [root_prop]
@@ -13,17 +13,17 @@ while len(prop_stack) > 0:
     if randint(0,depth_param) == 0:
         if first_rule == None:
             first_rule = Rule.Hyp_Intr
-        hypothesis.append(top)
+        if top != 'F':
+            hypothesis.append(top)
     else:   # Choose one applyable rule and create the child prop(s)
-        available_rules = [Rule.AND_Elim_L, Rule.AND_Elim_R, Rule.F_Elim]
-        if top == 'F':
-            available_rules.append(Rule.NOT_Elim)
+        if top in hypothesis:
+            available_rules = [Rule.Hyp_Intr]
+        elif top == 'F':
+            available_rules = [Rule.F_Elim,Rule.NOT_Elim]
         elif top[0] == '^':
-            available_rules.append(Rule.AND_Intr)
+            available_rules = [Rule.F_Elim,Rule.AND_Elim_L,Rule.AND_Elim_R,Rule.AND_Intr]
         elif top[0] == '~':
-            available_rules.append(Rule.NOT_Intr)
-        elif top in hypothesis:
-            available_rules.append(Rule.Hyp_Intr)
+            available_rules = [Rule.F_Elim,Rule.AND_Elim_L,Rule.AND_Elim_R,Rule.NOT_Intr]
         
         new_rule = available_rules[randint(0,len(available_rules)-1)]
         if first_rule == None:
@@ -34,14 +34,14 @@ while len(prop_stack) > 0:
         elif new_rule == Rule.NOT_Intr:
             prop_stack.append('F')
         elif new_rule == Rule.NOT_Elim:
-            new_prop = genProp(randint(5,prop_len/2))
+            new_prop = genProp(randint(5,prop_len))
             prop_stack.append(new_prop)
             prop_stack.append('~'+new_prop)
         elif new_rule == Rule.AND_Elim_L:
-            new_prop = genProp(randint(5,prop_len/2))
+            new_prop = genProp(randint(5,prop_len))
             prop_stack.append('^'+new_prop+top)
         elif new_rule == Rule.AND_Elim_R:
-            new_prop = genProp(randint(5,prop_len/2))
+            new_prop = genProp(randint(5,prop_len))
             prop_stack.append('^'+top+new_prop)
         elif new_rule == Rule.AND_Intr:
             prop_left, prop_right = getArgs(top)
